5fe98298eff4191864d6545422519caab402f22c
whuwuteng <714086885@qq.com>
2020-11-06 16:45:14 +0100

    example

---
+++ README.md
@@ -0,0 +1,9 @@
+# An adapter IO for Las File Read and Write
+
+An implement based on C++ and [lastool](https://github.com/LAStools/LAStools). 
+In this way, if lastool changes a lot, you do not need to change you kernl code.
+
+## Running environment 
+
+
+
---
+++ example/LasFileMethods.cpp
@@ -0,0 +1,55 @@
+
+#include <stdio.h>
+#include <string.h>
+
+#include <iostream>
+
+#include "WuLasLib.h"
+
+int main(int argc, char const *argv[])
+{
+	char szSrcLas[512] = "../data/Vaihingen_Strip_10-sub.LAS";
+	char szTarLas[512] = "../data/Vaihingen_Strip_10-cut.LAS";
+	
+	double minx = 497020;
+	double miny = 5420320;
+	double maxx = 497040;
+	double maxy = 5420420;
+	
+	CWuLasLib srcLas;
+	srcLas.Open(szSrcLas);
+	
+	int nPoints = srcLas.GetPtNum();
+	
+	DPT3D * pLas = new DPT3D[nPoints];
+	memset(pLas, 0, sizeof(DPT3D) * nPoints);
+	
+	srcLas.ReadLas(pLas, nPoints);
+	
+	double xOffset = 0;
+	double yOffset = 0;
+	double zOffset = 0;
+	srcLas.GetOffset(xOffset, yOffset, zOffset);
+	
+	double xPrecision = 0;
+	double yPrecision = 0;
+	double zPrecision = 0;
+	srcLas.GetPrecision(xPrecision, yPrecision, zPrecision);
+	srcLas.Close();
+	
+	CWuLasLib tarLas;
+	tarLas.Open(szTarLas, CWuLasLib::modeWrite);
+	tarLas.SetOffset(xOffset, yOffset, zOffset);
+	tarLas.SetPrecision(xPrecision, yPrecision, zPrecision);
+	
+	for(int i = 0; i < nPoints; ++i){
+		if (pLas[i].x > minx && pLas[i].x < maxx && \
+			pLas[i].y > miny && pLas[i].y < maxy){
+			tarLas.WriteLas(pLas + i, 1);
+		}
+	}
+	tarLas.Close();
+	
+	delete []pLas;
+	return 0;
+}
